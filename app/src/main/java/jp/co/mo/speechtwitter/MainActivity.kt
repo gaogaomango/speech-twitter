29284da4f3b2f628faa7457884bfc480ebc00b26
Ogai Masataka <gaogaomango@gmail.com>
2018-12-21 00:36:18 +1100

    set speech speed

--- app/src/main/java/jp/co/mo/speechtwitter/MainActivity.kt
+++ app/src/main/java/jp/co/mo/speechtwitter/MainActivity.kt
@@ -6,6 +6,7 @@ import android.speech.tts.TextToSpeech
 import android.speech.tts.UtteranceProgressListener
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,10 @@ class MainActivity : AppCompatActivity() {
     private val TAG = MainActivity::class.simpleName
 
     private lateinit var tts: TextToSpeech
+
+    private val SPEECH_MAX_SPEED: Int = 40
+    private val SPEECH_MIN_SPEED: Int = 1
+    private var speechSpeed: Int = 10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +33,30 @@ class MainActivity : AppCompatActivity() {
 
         this.btnSpeechText.setOnClickListener({
             speechText()
+        })
+
+        this.speechSpeedSeekBar.max = SPEECH_MAX_SPEED
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            this.speechSpeedSeekBar.min = SPEECH_MIN_SPEED
+        }
+        this.speechSpeedSeekBar.progress = speechSpeed
+        this.speechSpeedSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                speechSpeed = progress
+                setSpeehRate(speechSpeed)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // TODO: pause
+                if (tts.isSpeaking()) {
+                    tts.stop()
+                }
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // TODO: restart
+            }
+
         })
 
 
@@ -46,20 +75,17 @@ class MainActivity : AppCompatActivity() {
         sampleText.selectAll()
         val speechTxt: String? = sampleText.text.toString()
 
-        if(tts.isSpeaking()) {
+        if (tts.isSpeaking()) {
             tts.stop()
             return
         }
 
-        tts.setSpeechRate(1.0f)
+        setSpeehRate(speechSpeed)
         tts.setPitch(1.0f)
 
-        if(Build.VERSION.SDK_INT >= 21) {
+        if (Build.VERSION.SDK_INT >= 21) {
             tts.speak(speechTxt, TextToSpeech.QUEUE_FLUSH, null, "messageID")
         } else {
-//            val map = mutableMapOf<String, String>()
-//            map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "messageID")
-//            tts.speak(speechTxt, TextToSpeech.QUEUE_FLUSH, map)
             val map = HashMap<String, String>()
             map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "messageID")
             tts.speak(speechTxt, TextToSpeech.QUEUE_FLUSH, map)
@@ -67,9 +93,14 @@ class MainActivity : AppCompatActivity() {
         setTtsListener()
     }
 
+    private fun setSpeehRate(speedInt: Int) {
+        val speed: Float = speedInt.toFloat() / 10
+        tts.setSpeechRate(speed)
+    }
+
     private fun setTtsListener() {
-        if(Build.VERSION.SDK_INT >= 15) {
-            val listerResult = tts.setOnUtteranceProgressListener(object: UtteranceProgressListener(){
+        if (Build.VERSION.SDK_INT >= 15) {
+            val listerResult = tts.setOnUtteranceProgressListener(object : UtteranceProgressListener() {
                 override fun onDone(utteranceId: String?) {
                     Log.e(TAG, "progress on Done $utteranceId")
                 }
